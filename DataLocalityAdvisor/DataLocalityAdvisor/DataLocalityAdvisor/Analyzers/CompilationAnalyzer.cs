1fa1b38b1eb7b85c5b1b1704833a71363bf32d33
Nathan Santanna <nathansantanna@gmail.com>
2018-09-25 17:05:10 -0300

    Tests And Structure Going on

--- DataLocalityAdvisor/DataLocalityAdvisor/DataLocalityAdvisor/Analyzers/CompilationAnalyzer.cs
+++ DataLocalityAdvisor/DataLocalityAdvisor/DataLocalityAdvisor/Analyzers/CompilationAnalyzer.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
 using System.Collections.Generic;
+
 namespace DataLocalityAnalyzer
 {
     class CompilationAnalyzer
     {
+        
         private HashSet<string> _collections;
 
-        
+        internal void SemanticAction(SemanticModelAnalysisContext semanticModelAnalysisContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ISymbol> GetCollections(SemanticModelAnalysisContext semanticModelAnalysisContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void EndcompilationAction(CompilationAnalysisContext obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- DataLocalityAdvisor/DataLocalityAdvisor/DataLocalityAdvisor/RoslynCommuncationAgent.cs
+++ DataLocalityAdvisor/DataLocalityAdvisor/DataLocalityAdvisor/RoslynCommuncationAgent.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using DataLocalityAnalyzer;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Operations;
@@ -19,7 +20,7 @@ namespace DataLocalityAdvisor
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
         private const string Category = "Data Locality";
         #endregion 
-        //public to help to test the diagnosis
+        //public to help to  unit test the diagnosis
         public static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, true, Description);
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
@@ -27,6 +28,16 @@ namespace DataLocalityAdvisor
         public override void Initialize(AnalysisContext context)
         {
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterCompilationStartAction( compilationContext  =>
+            {
+                INamedTypeSymbol collectionsNameSpace = compilationContext.Compilation.GetTypeByMetadataName("System.Collections") ;
+                if (collectionsNameSpace == null)
+                    return;
+                CompilationAnalyzer analyzer = new CompilationAnalyzer();
+                compilationContext.RegisterSemanticModelAction(analyzer.SemanticAction);
+                compilationContext.RegisterCompilationEndAction(analyzer.EndcompilationAction);
+            });
         }
 
     }
